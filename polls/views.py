4d19e6bad945fb2936e9c32ebeec70d957954f8f
khan <khan@gmail.com>
2021-08-30 17:41:18 +0500

    'django-project-1'

---
+++ polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+   
+
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def blog(request,id):
+  
+    return HttpResponse('This is a blog')
+
